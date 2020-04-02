406d54c268165ce1ef8544557de53afc99a8902c
marcelo escamilla <mescamillas@unal.edu.co>
2020-04-01 21:25:31 -0500

    Primera prueba de concepto

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # analizador-lexico
 analizador lexico para el lenguaje chocopy. taller para la materia lenguajes de programacion 2020-1
+___
+## update 1
+- el archivo test.txt contiene el codigo de ejemplo del enunciado
+
+- Se hizo una prueba de concepto para identificar operadores de un solo caracter +%%()[],.:
+
+- Se implemento la clase Position que guarda el indice, linea y columna del caracter que se lee.
+   Esta clase contiene el indice(0-indexado), la fila y la columna(ambos 1-indexado) del caracter actual.  
+   Una posible idea es que el Token en si sea un objeto con informacion util. El indice es util de manera logica, puesto que el archivo se carga como un arreglo de caracteres. la fila y columna permiten determinar facilmente en donde se encuentra el caracter. Por ahora, la iteracion de caracteres esta despegada de esta clase, sigue siendo una prueba de concepto.
+
+- Se creo la clase Token como un contenedor para los tokens(no se implementa aun)
---
+++ lexer.py
@@ -0,0 +1,49 @@
+# Guarda el indice, linea y columna del caracter
+class Position:
+    def __init__(self, index, line, col):
+        self.index = index
+        self.line = line
+        self.col = col
+
+    def __str__(self):
+        return f'idx: {self.index}, line: {self.line}, col: {self.col} '
+
+    def advance(self, current_char):
+        self.index += 1
+        self.col += 1
+
+        if current_char == '\n':
+            self.line +=1
+            self.col = 0
+
+        return self
+
+    def copy(self):
+        return Position(self.index,self.line,self.col)
+
+class Token:
+    def __init__(self, type_, value=None):
+        self.type = type_
+        self.value = value
+
+    def __str__(self):
+        if self.value:
+            return f'{self.type}, {self.value}'
+        return f'{self.type}'
+
+f = open("test.txt","r")
+text = f.read()
+pos = Position(0,1,1)
+for char in text:
+    if char == '\n': print(f'>\\n<',pos)
+    elif char == '+': print(f'<tk_sum, {pos.line}, {pos.col}>')
+    elif char == '%': print(f'<tk_mod, {pos.line}, {pos.col}>')
+    elif char == '(': print(f'<tk_par_izq, {pos.line}, {pos.col}>')
+    elif char == ')': print(f'<tk_par_der, {pos.line}, {pos.col}>')
+    elif char == '[': print(f'<tk_llave_izq, {pos.line}, {pos.col}>')
+    elif char == ']': print(f'<tk_llave_der, {pos.line}, {pos.col}>')
+    elif char == ',': print(f'<tk_coma, {pos.line}, {pos.col}>')
+    elif char == '.': print(f'<tk_punto, {pos.line}, {pos.col}>')
+    elif char == ':': print(f'<tk_dospuntos, {pos.line}, {pos.col}>')
+    else: print(f'>{char}<',pos)
+    pos.advance(char)

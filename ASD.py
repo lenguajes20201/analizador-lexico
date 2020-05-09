60be4e7bec993d83028cc91939f2ebce57d0edd3
Ivan <irdelgadog@unal.edu.co>
2020-05-08 21:22:50 -0500

    build grammar

--- ASD.py
+++ ASD.py
@@ -1,4 +1,160 @@
 chocopySyntax = {
+    #program ::= [var def | func def | class def]^∗ stmt^∗
+    'program':{
+        'nt_inicio_def nt_inicio_stmt',
+        
+    },
+
+    'nt_inicio_def':{
+        'nt_inicio_def nt_var_def',
+        'nt_inicio_def nt_func_def',
+        'nt_inicio_def nt_class_def',
+        'e'
+    },
+
+    'nt_inicio_stmt':{
+        'nt_inicio_stmt nt_stmt',
+        'e'
+    },
+
+    #class def ::= class ID ( ID ) : NEWLINE INDENT class body DEDENT
+    'nt_class_def':{
+        'kw_class tk_id tk_par_izq tk_id tk_par_der NEWLINE INDENT nt_class_body DEDENT',
+    },
+
+    #class_body ::= pass NEWLINE | [ var def | func def ]^+
+    'nt_class_body':{
+        'kw_pass NEW_LINE',
+        'nt_aux_def nt_var_def',
+        'nt_aux_def nt_func_def'
+    },
+
+    'nt_aux_def':{
+        'nt_aux_def nt_var_def',
+        'nt_aux_def nt_func_def',
+        'e'
+    },
+
+    #func_def ::= def ID ( [typed_var [, typed_var]^∗]^? ) [-> type ]^? : NEWLINE INDENT func_body DEDENT    
+
+    'nt_fun_def':{
+        'kw_def tk_id tk_par_izq nt_argument_aux tk_par_der nt_return_aux tk_dospuntos NEWLINE INDENT nt_func_body DEDENT'
+    },
+
+    'nt_argument_aux':{
+       'nt_typed_var nt_nvars_aux',
+       'e'
+    },
+
+    'nt_nvars_aux':{
+        'nt_nvars_aux tk_coma nt_typed_var',
+        'e'
+    },
+    
+    'nt_return_aux':{
+        'tk_ejecuta nt_type',
+        'e'
+    },
+
+    # func_body ::= [global_decl | nonlocal_decl | var_def | func_def ]^* stmt^+
+
+    'nt_func_body':{
+        'nt_decl_def_aux nt_nstmt_aux nt_stmt'
+    },
+
+    'nt_decl_def_aux':{
+        'nt_decl_def_aux nt_global_decl',
+        'nt_decl_def_aux nt_nonlocal_decl',
+        'nt_decl_def_aux nt_var_def',
+        'nt_decl_def_aux nt_func_def',
+        'e',
+    },
+
+    'nt_nstmt_aux':{
+        'nt_nstmt_aux nt_stmt',
+        'e'
+    },
+
+    # typed_var ::= ID : type
+
+    'nt_typed_var':{
+        'tk_id tk_dospuntos nt_type'
+    },
+
+    # type ::= ID | IDSTRING | [ type ]
+
+    'nt_type':{
+        'tk_id',
+        'tk_idstring',
+        'tk_llave_izq nt_type tk_llave_der'
+    },
+
+    #global decl ::= global ID NEWLINE
+
+    'nt_global_decl':{
+        'kw_global tk_id NEWLINE'
+    },
+
+    #nonlocal decl ::= nonlocal ID NEWLINE
+
+    'nt_nonlocal_decl':{
+        'kw_nonlocal tk_id NEWLINE'
+    },
+
+    #var_def ::= typed_var = literal NEWLINE
+
+    'nt_var_def':{
+        'nt_typed_var tk_asig nt_literal NEWLINE'
+    },
+
+    # stmt ::= simple_stmt NEWLINE | if expr : block [elif expr : block ]^* [else : block]^? | while expr : block | for ID in expr : block
+
+    'nt_stmt':{
+        'nt_simple_stmt NEWLINE',
+        'kw_if nt_expr tk_dospuntos nt_block nt_ncond_aux',
+        'nt_fcond_aux',
+        'kw_while nt_expr tk_dospuntos nt_block',
+        'kw_for tk_id kw_in nt_expr tk_dospuntos nt_block'
+    },
+
+    'nt_ncond_aux':{
+        'nt_ncond_aux kw_elif nt_expr tk_dospuntos nt_block',
+        'e'
+    },
+
+    'nt_fcond_aux':{
+        'kw_else tk_dospuntos nt_block',
+        'e'
+    },
+
+    # simple stmt ::= pass | expr | return [expr]^? | [ target = ]^+ expr
+
+    'nt_simple_stmt':{
+        'kw_pass',
+        'nt_expr',
+        'kw_return nt_expr_aux',
+        'nt_ntargets_aux nt_target tk_asig nt_expr'
+
+    },
+
+    'nt_expr_aux':{
+        'nt_expr',
+        'e'
+    },
+
+    'nt_ntargets_aux':{
+        'nt_ntargets_aux nt_target tk_asig',
+        'e'
+    },
+
+    #block ::= NEWLINE INDENT stmt+ DEDENT
+
+    'nt_block':{
+        'NEWLINE INDENT nt_nstmt_aux nt_stmt DEDENT'
+    },
+
+
+
     'nt_literal': {
         'kw_NONE',
         'kw_True',
@@ -60,5 +216,9 @@ chocopySyntax = {
         'nt_member_expr',
         'nt_index_expr'
     }
+}
+
+otro : {
+    
 }
 print(chocopySyntax)
